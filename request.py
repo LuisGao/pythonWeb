55f7f094d69c015185e34b9da5baf4844b045d49
unknown <LuisGao>
2020-03-13 23:05:17 +0800

    1123

--- request.py
+++ request.py
@@ -10,9 +10,22 @@ headers = {
     'referer': 'https://www.geekdigging.com/'
 }
 
-resp = requests.get('https://httpbin.org/get', params, headers=headers)
-print(resp.text)
-print(resp.json())
-print(type(resp.text))
+proxies_socket = {
+    'http': 'socks5://user:pass@host:port',
+    'https': 'socks5://user:pass@host:port'
+}
 
+# resp = requests.get('https://httpbin.org/get', params, headers=headers)
+# print(resp.text)
+# print(resp.json())
+# print(type(resp.text))
+
+# 利用requests库获取资源
+# res = requests.get("https://www.baidu.com/img/superlogo_c4d7df0a003d3db9b65e9ef0fe6da1ec.png")
+# with open('F:\\baidu_logo.png', 'wb') as f:
+#     f.write(res.content)
+#     f.close
+
+resp = requests.post('https://httpbin.org/post', data=params, headers=headers)
+print(resp.text)
 
