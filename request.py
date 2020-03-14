8b890e34085ac734f1810e26d6fd7bf9b21b4223
unknown <LuisGao>
2020-03-14 11:14:09 +0800

    西安二手房交易信息爬虫：未破解反爬虫机制

--- request.py
+++ request.py
@@ -1,4 +1,7 @@
 import requests
+import time
+from lxml import etree
+import xlwt
 
 params = {
     'name':'geekdigging',
@@ -26,6 +29,66 @@ proxies_socket = {
 #     f.write(res.content)
 #     f.close
 
-resp = requests.post('https://httpbin.org/post', data=params, headers=headers)
-print(resp.text)
+# resp = requests.post('https://httpbin.org/post', data=params, headers=headers)
+# print(resp.text)
+# print(resp.cookies)
 
+headers = {
+    'host':'xa.ke.com',
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; …) Gecko/20100101 Firefox/74.0'
+}
+
+TITLES = []
+PRICES = []
+
+def getListPage(url):
+    resp = requests.get(url)
+    doc = resp.content.decode('utf-8')
+    # print(resp.content.decode('utf-8'))
+
+    # get response body
+    html = etree.HTML(doc)
+    # get folder list
+    title_list = html.xpath('/html/body/div[1]/div[5]/div[1]/div[4]/ul/li/div[1]/div[1]/a/text()')
+    price_list = html.xpath('/html/body/div[1]/div[5]/div[1]/div[4]/ul/li/div[1]/div[2]/div[@class="totalPrice"]/span/text()')
+
+    for i in range(len(title_list)):
+        TITLES.append(title_list[i])
+        PRICES.append(price_list[i])
+
+    print(len(title_list))
+    if len(title_list) > 0:
+        print('page: ' + url + ' get Success!')
+    else:
+        print('page: ' + url + ' get Error!')
+
+    # get link list
+    # price_list = html.xpath('//*[@id="pins"]/li/a/@href')
+
+def writeToExcel():
+    workbook = xlwt.Workbook()
+    sheet1 = workbook.add_sheet('sheet1', cell_overwrite_ok=True)
+    for i in range(len(TITLES)):
+        sheet1.write(i, 0, TITLES[i])
+        sheet1.write(i, 1, PRICES[i])
+    workbook.save('F:\\西安二手房交易记录.xls')
+    print('二手房数据保存完毕')
+
+def main():
+
+    getListPage('https://xa.ke.com/chengjiao/')
+    for i in range(100):
+        if i == 0:
+            continue
+        time.sleep(3)
+        getListPage('https://xa.ke.com/chengjiao/pg' + str(i + 1) + '/')    
+    writeToExcel()
+
+if __name__ == '__main__':
+    TITLES = ['基本信息']
+    PRICES = ['成交价格']
+    # writeToExcel()
+    main()
+
+# r = requests.get('https://xa.ke.com/chengjiao/pg100/')
+# print(r.content)
